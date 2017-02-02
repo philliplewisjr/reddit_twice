0aaaff01d99f1d551f366899fe781d74fdb791ad
Phillip Lewis <phillip.lewisjr@gmail.com>
2017-02-02 13:21:16 -0600

    vote button simi complete

--- controllers/MyPosts.js
+++ controllers/MyPosts.js
@@ -16,10 +16,26 @@ app.controller('MyPostsCtrl', function($scope, $http, postFactory){
     console.log("thumbs up")
     console.log(key)
     console.log(value)
+    value.counter += 1
+    // value.img = ""
+    // value.title = ""
+    let counterObj = {
+    counter: value.counter
+    }
+
+    $http.patch(`https://reddit-remake.firebaseio.com/-KbqByZHKlsW1W0RI4DR/newPost/${key}/.json`, JSON.stringify(counterObj))
   }
 
-  $scope.thumbsDown = (key)=> {
+  $scope.thumbsDown = (key, value)=> {
     console.log("thumbs down")
-  }
+    console.log(key)
+    console.log(value)
+    value.counter -= 1
+     let counterObj = {
+    counter: value.counter
+    }
 
+    $http.patch(`https://reddit-remake.firebaseio.com/-KbqByZHKlsW1W0RI4DR/newPost/${key}/.json`, JSON.stringify(counterObj))
+  
+  }
 })
